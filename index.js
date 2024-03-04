925b1f7592e09ea7246930420e22a7fb3b41389a
Saket Prasad <saket98838@gmail.com>
2024-03-04 09:51:31 +0530

    Made JS code more efficient.

--- index.js
+++ index.js
@@ -5,13 +5,26 @@ let password1 = document.getElementById("password-1")
 let password2 = document.getElementById("password-2")
 let generatePassword = document.getElementById("generate-password")
 
+// 1st time.
+// generatePassword.addEventListener("click", function() {
+//     password1.textContent = ""
+//     password2.textContent = ""
+//     for (i = 1; i < 16; i++) {
+//         let passwordLeft = Math.floor( Math.random() * characters.length )
+//         let passwordRight = Math.floor( Math.random() * characters.length )
+//         password1.textContent += characters[passwordLeft]
+//         password2.textContent += characters[passwordRight]
+//     }
+// })
+
+// 2nd time more efficient code.
 generatePassword.addEventListener("click", function() {
-    password1.textContent = ""
-    password2.textContent = ""
-    for (i = 1; i < 16; i++) {
-        let passwordLeft = Math.floor( Math.random() * characters.length )
-        let passwordRight = Math.floor( Math.random() * characters.length )
-        password1.textContent += characters[passwordLeft]
-        password2.textContent += characters[passwordRight]
+    let pass1 = ""
+    let pass2 = ""
+    for (i = 0; i < 15; i++) {
+        pass1 += characters[Math.floor( Math.random() * characters.length )]
+        pass2 += characters[Math.floor( Math.random() * characters.length )]
     }
+    password1.textContent = pass1
+    password2.textContent = pass2
 })
